60d13c1cba5333b7e34a9c60a4e09326176ee773
Figglewatts <figglewatts@hotmail.co.uk>
2017-11-05 20:57:02 +0000

    Added OpenTK

--- src/view/LSDViewForm.Designer.cs
+++ src/view/LSDViewForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LSDView
+﻿namespace LSDView.view
 {
     partial class LSDViewForm
     {
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.redButton = new System.Windows.Forms.Button();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.viewingWindow = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // redButton
+            // 
+            this.redButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.redButton.Location = new System.Drawing.Point(639, 13);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(133, 23);
+            this.redButton.TabIndex = 1;
+            this.redButton.Text = "Red";
+            this.redButton.UseVisualStyleBackColor = true;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.greenButton.Location = new System.Drawing.Point(639, 43);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(133, 23);
+            this.greenButton.TabIndex = 2;
+            this.greenButton.Text = "Green";
+            this.greenButton.UseVisualStyleBackColor = true;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.blueButton.Location = new System.Drawing.Point(639, 73);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(133, 23);
+            this.blueButton.TabIndex = 3;
+            this.blueButton.Text = "Blue";
+            this.blueButton.UseVisualStyleBackColor = true;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // viewingWindow
+            // 
+            this.viewingWindow.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.viewingWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewingWindow.Location = new System.Drawing.Point(0, 0);
+            this.viewingWindow.Name = "viewingWindow";
+            this.viewingWindow.Size = new System.Drawing.Size(781, 564);
+            this.viewingWindow.TabIndex = 0;
+            this.viewingWindow.VSync = false;
+            this.viewingWindow.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+            this.viewingWindow.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl_KeyDown);
+            this.viewingWindow.Resize += new System.EventHandler(this.glControl_Resize);
+            // 
+            // LSDViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(781, 564);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.viewingWindow);
+            this.Name = "LSDViewForm";
+            this.Text = "OpenTK Windows Forms Tutorial 01 - Your first window";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private OpenTK.GLControl viewingWindow;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button blueButton;
     }
 }
-
--- src/view/LSDViewForm.cs
+++ src/view/LSDViewForm.cs
@@ -1,20 +1,251 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
 
-namespace LSDView
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform;
+using OpenTK;
+
+#endregion
+
+namespace LSDView.view
 {
     public partial class LSDViewForm : Form
     {
+        static float angle = 0.0f;
+
+        #region --- Constructor ---
+
         public LSDViewForm()
         {
             InitializeComponent();
+        }
+
+        #endregion
+
+        #region OnLoad
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            viewingWindow.KeyDown += new KeyEventHandler(glControl_KeyDown);
+            viewingWindow.KeyUp += new KeyEventHandler(glControl_KeyUp);
+            viewingWindow.Resize += new EventHandler(glControl_Resize);
+            viewingWindow.Paint += new PaintEventHandler(glControl_Paint);
+
+            Text =
+                GL.GetString(StringName.Vendor) + " " +
+                GL.GetString(StringName.Renderer) + " " +
+                GL.GetString(StringName.Version);
+
+            GL.ClearColor(Color.MidnightBlue);
+            GL.Enable(EnableCap.DepthTest);
+
+            Application.Idle += Application_Idle;
+
+            // Ensure that the viewport and projection matrix are set correctly.
+            glControl_Resize(viewingWindow, EventArgs.Empty);
+        }
+
+        void glControl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F12)
+            {
+                GrabScreenshot().Save("screenshot.png");
+            }
+        }
+
+        #endregion
+
+        #region OnClosing
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Application.Idle -= Application_Idle;
+
+            base.OnClosing(e);
+        }
+
+        #endregion
+
+        #region Application_Idle event
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (viewingWindow.IsIdle)
+            {
+                Render();
+            }
+        }
+
+        #endregion
+
+        #region GLControl.Resize event handler
+
+        void glControl_Resize(object sender, EventArgs e)
+        {
+            OpenTK.GLControl c = sender as OpenTK.GLControl;
+
+            if (c.ClientSize.Height == 0)
+                c.ClientSize = new System.Drawing.Size(c.ClientSize.Width, 1);
+
+            GL.Viewport(0, 0, c.ClientSize.Width, c.ClientSize.Height);
+
+            float aspect_ratio = Width / (float)Height;
+            Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perpective);
+        }
+
+        #endregion
+
+        #region GLControl.KeyDown event handler
+
+        void glControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Escape:
+                    this.Close();
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region GLControl.Paint event handler
+
+        void glControl_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        #endregion
+
+        #region private void Render()
+
+        private void Render()
+        {
+            Matrix4 lookat = Matrix4.LookAt(0, 5, 5, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+            angle += 0.05f;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            DrawCube();
+
+            viewingWindow.SwapBuffers();
+        }
+
+        #endregion
+
+        #region private void DrawCube()
+
+        private void DrawCube()
+        {
+            GL.Begin(BeginMode.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+
+        #endregion
+
+        #region private void GrabScreenshot()
+
+        Bitmap GrabScreenshot()
+        {
+            Bitmap bmp = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            System.Drawing.Imaging.BitmapData data =
+            bmp.LockBits(this.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GL.ReadPixels(0, 0, this.ClientSize.Width, this.ClientSize.Height, PixelFormat.Bgr, PixelType.UnsignedByte,
+                data.Scan0);
+            bmp.UnlockBits(data);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            return bmp;
+        }
+
+        #endregion
+
+        #region public static void Main()
+
+        /// <summary>
+        /// Entry point of this example.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            OpenTK.Toolkit.Init();
+            using (LSDViewForm example = new LSDViewForm())
+            {
+                example.ShowDialog();
+            }
+        }
+
+        #endregion
+
+        private void redButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
